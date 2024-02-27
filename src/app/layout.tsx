a248193ba8de584aff44dfd9da446b6149c51645
ShampaShaw <shampashaw088@gmail.com>
2024-02-27 23:18:45 +0530

    Add navbar and footer components to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,18 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from '@/components/navbar/Navbar'
+import Footer from '@/components/footer/Footer'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      {children}
-    </div>
+    <html lang='en'>
+      <body className='bg-white text-black'>
+        <div>
+          <Navbar/>
+          {children}
+          <Footer/>
+        </div>
+      </body>
+    </html>
   )
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className='bg-gray-800 text-white text-center p-4'>
+        <p>Footer</p>
+        </footer>
+    )
+}
+
+export default Footer;
