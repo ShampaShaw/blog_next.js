839269d0511f325fb297fd5e895667da96b2feb2
ShampaShaw <shampashaw088@gmail.com>
2024-03-09 07:47:25 +0530

    css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,17 @@ import styles from './home.module.css'
 const Home = () => {
   return <div className={styles.container}>
     <div className={styles.textContainer}>
-      <h1>Imagination Unleashed: Where Creativity Finds Its Voice</h1>
-      <p>Embark on a journey through Creative Chronicles, a haven where thoughts transcend into vibrant narratives. Join us in celebrating the limitless world of creative expression and let your ideas find resonance in our community of imaginative minds.</p>
+    <h1 className='text-5xl font-bold'>Imagination Unleashed: Where Creativity Finds Its Voice</h1>
+      <p className={styles.desc}>Embark on a journey through Creative Chronicles, a haven where thoughts transcend into vibrant narratives. Join us in celebrating the limitless world of creative expression and let your ideas find resonance in our community of imaginative minds.</p>
       <div className={styles.buttons}>
         <button className={styles.button}>Learn More</button>
         <button className={styles.button}>Contact</button>
       </div>
-      <div className={styles.brand}>
+      <div className={styles.brands}>
         <Image src="/brands.png" alt='' fill className={styles.brandImg}/>
       </div>
     </div>
-    <div className={styles.imageContainer}>
+    <div className={styles.imgContainer}>
       <Image src="/hero.gif" alt='' fill className={styles.heroImg}/>
     </div>
   </div>;
